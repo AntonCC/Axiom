1d5872a91ef8e512d0441f3d90e8b9ec7b16b670
Anton Chetvertkov <antonchetvv@gmail.com>
2020-08-31 17:54:16 -0400

    api req handling

--- src/components/payment-modal/payment-modal.component.jsx
+++ src/components/payment-modal/payment-modal.component.jsx
@@ -8,7 +8,8 @@ import { clearCart } from '../../redux/cart/cart.actions'
 import { ReactComponent as Exit } from '../../imgs/times-circle-regular.svg'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://antonchet.com:5050/api',
+  headers: {'Access-Control-Allow-Origin': '*'}
 })
 
 const cardOptions = {
@@ -33,33 +34,31 @@ const PaymentModal = ({ orderTotal, closeModal, clearCart }) => {
 
   const handleSubmit = async e => {
     e.preventDefault()
-    const res = await api.get('/test')
-    console.log(res)
-    // const res = await api.post('/')
-    // const clientSecret = res.data.client_secret
+    const res = await api.post('/secret')
+    const clientSecret = res.data.client_secret
 
-    // const cardElement = elements.getElement(CardElement)
+    const cardElement = elements.getElement(CardElement)
 
-    // stripe.confirmCardPayment(clientSecret, {
-    //   payment_method: {
-    //     card: cardElement,
-    //     billing_details: {
-    //       name: formDetails.name,
-    //       address: formDetails.address,
-    //       city: formDetails.city,
-    //       // state: formDetails.state,
-    //     }
-    //   }
-    // }).then(result => {
-    //   if(result.error) {
-    //     console.log(result.error.message)
-    //   } else if(result.paymentIntent.status === 'succeeded') {
-    //     console.log("Payment successful.")
-    //     closeModal()
-    //     alert("Payment Successfull.")
-    //     clearCart()
-    //   }
-    // })
+    stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: cardElement,
+        billing_details: {
+          name: formDetails.name,
+          address: formDetails.address,
+          city: formDetails.city,
+          // state: formDetails.state,
+        }
+      }
+    }).then(result => {
+      if(result.error) {
+        console.log(result.error.message)
+      } else if(result.paymentIntent.status === 'succeeded') {
+        console.log("Payment successful.")
+        closeModal()
+        alert("Payment Successfull.")
+        clearCart()
+      }
+    })
   }
 
   const handleChange = e => {
