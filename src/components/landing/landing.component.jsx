b83ddca7737af33c1bc6c21de3aed04c054b6d13
Anton Chetvertkov <antonchetvv@gmail.com>
2020-09-10 14:23:40 -0400

    responsiveness fixes on landing

--- src/components/landing/landing.component.jsx
+++ src/components/landing/landing.component.jsx
@@ -11,8 +11,8 @@ const Landing = () => {
           <h2>Headphones for <strong className="underline">all</strong> your music</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel tempore aliquid deleniti repellendus qui commodi?</p>
           <div className="landing-cta">
-            <RoundedButton className="landing-btn" btnPath="/about" btnText="Learn More" /> 
-            <RoundedButton className="landing-btn" btnPath="/buy" btnText="View Models" inverse />        
+            <RoundedButton btnPath="/about" btnText="Learn More" /> 
+            <RoundedButton btnPath="/buy" btnText="View Models" inverse />        
           </div>
         </div>
         <div className="side-b">
